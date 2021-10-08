7caa60dd5727e8d0a972c47c7f5a23cc6c88f4c0
Mateus Sampaio <mateus4k@protonmail.ch>
2021-10-08 12:32:10 -0300

    Initial commit

---
+++ src/server.test.js
@@ -0,0 +1,7 @@
+const supertest = require('supertest');
+const server = require('./server');
+
+test('GET / returns a message', async () => {
+  const result = await supertest(server).get('/').expect(200);
+  expect(result.body.message).toEqual('Hello World!');
+});
