41f31727ca3cebd59f7c72e98641c0c77aa6974a
Mateus Sampaio <mateus4k@protonmail.ch>
2021-10-08 13:26:30 -0300

    Update message

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send({ message: 'Hello, PFK 2!' });
+  res.send({ message: 'Hello World!' });
 });
 
 module.exports = app;
--- src/server.test.js
+++ src/server.test.js
@@ -3,5 +3,5 @@ const server = require('./server');
 
 test('GET / returns a message', async () => {
   const result = await supertest(server).get('/').expect(200);
-  expect(result.body.message).toEqual('Hello, PFK 2!');
+  expect(result.body.message).toEqual('Hello World!');
 });
